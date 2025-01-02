3d1e88579abdb96f1274d8fba1ca18ce6065c2cd
krudda <krudda@yandex.ru>
2025-01-03 00:23:36 +0300

    First API call

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello world from OpenAI course!");
+import { apiRequest } from './apiRequest'
+
+const userQuestion = 'Provide me advise what exercises should I do for my back pain?';
+
+async function main() {
+    const response = await apiRequest(userQuestion);
+    console.log(response);
+};
+
+main();
