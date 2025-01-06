90b0162978f349cbf914224f27dfe7aa84c1b4d5
Dmitrii <43067932+Krudda@users.noreply.github.com>
2025-01-06 22:47:36 +0300

    module1/3-roles (#2)
    * Add Roles
    
    * Add Roles

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import { apiRequest } from './apiRequest'
 
-const userQuestion = 'Provide me advise what exercises should I do for my back pain?';
+const context = "You are a health coach and fitness trainer.";
+const userQuestion = "Provide me advise what exercises should I do for my back pain?";
 
 async function main() {
-    const response = await apiRequest(userQuestion);
+    const response = await apiRequest({userQuestion, context});
     console.log(response);
-};
+}
 
 main();
